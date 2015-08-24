ecf5977843ed25153efecf5ef69dd8f0e96a9f73
Dan Menard <dan.menard@jadedpixel.com>
2015-08-24 14:55:58 -0400

    Use more result data and some basic no-nonsense styles.

--- index.ios.js
+++ index.ios.js
@@ -11,6 +11,7 @@ var {
   Text,
   TextInput,
   View,
+  ScrollView,
 } = React;
 
 var WORDNIK_URL_BASE = 'http://api.wordnik.com:80/v4/word.json/';
@@ -20,7 +21,7 @@ var Dictionary = React.createClass({
   getInitialState: function() {
     return {
       inputText: '',
-      resultText: ''
+      resultJSX: <Text></Text>
     };
   },
 
@@ -35,15 +36,28 @@ var Dictionary = React.createClass({
   lookUp: function(text) {
     this.setState((state) => {
       return {
-        resultText: 'Loooking up "' + state.inputText + '"...'
+        resultJSX: <Text>{'Loooking up "' + state.inputText + '"...'}</Text>
       };
     });
 
     fetch(WORDNIK_URL_BASE + text + "/definitions?api_key=" + SECRETS.api_key)
       .then((response) => {
         this.setState((state) => {
+          var results = [];
+
+          for (let result of JSON.parse(response._bodyText)) {
+            results.push(
+              <View style={styles.container}>
+                <Text style={styles.definitionType}>{result.partOfSpeech}</Text>
+                <Text style={styles.definition}>{result.text}</Text>
+                <Text style={styles.definitionSource}>{result.attributionText}</Text>
+              </View>
+            );
+          }
+
           return {
-            resultText: JSON.parse(response._bodyText)[0].text
+            resultJSX:
+              <ScrollView>{results}</ScrollView>
           };
         });
       })
@@ -52,34 +66,49 @@ var Dictionary = React.createClass({
 
   render: function() {
     return (
-      <View style={styles.container}>
+      <ScrollView style={[styles.container, styles['container--main']]}>
         <TextInput
           autoCapitalize="none"
-          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          style={styles.wordPrompt}
           value={this.state.inputText}
           onChange={(event) => this.updateText(event.nativeEvent.text)}
           onEndEditing={() => this.lookUp(this.state.inputText)}
         />
 
-        <Text style={styles.instructions}>
-          {this.state.resultText}
-        </Text>
-      </View>
+        {this.state.resultJSX}
+      </ScrollView>
     );
   },
 });
 
 var styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    marginTop: 20
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  'container--main': {
+    margin: 20,
+    marginTop: 40
+  },
+
+  wordPrompt: {
+    height: 40,
+    paddingLeft: 10,
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+
+  definition: {
+    marginTop: 5,
+    marginBottom: 5
+  },
+
+  definitionType: {
+    fontStyle: 'italic'
+  },
+
+  definitionSource: {
+    color: 'gray'
   }
 });
 
